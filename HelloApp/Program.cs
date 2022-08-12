6ad4ea0b348382ee22e13bfd9c276a59e23138e9
Raghav Ambashta <raghav.ambashta22@gmail.com>
2022-08-12 14:35:47 +0530

    Added new project

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -7,8 +7,14 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
+            Console.WriteLine("Hello User!");
+            Console.WriteLine("Welcome to Jenkins");
+
+            Calc calc = new Calc();
+
+            Console.WriteLine($"Sum = {calc.Add(70, 80)}");
+            Console.WriteLine($"Multiply = {calc.Multiply(4, 5)}");
+            Console.ReadLine();
         }
     }
 }
