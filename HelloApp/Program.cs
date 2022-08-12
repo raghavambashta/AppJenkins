938fd2ef0ef7aa53dabe9a353d4292e0d1f49c59
Raghav Ambashta <raghav.ambashta22@gmail.com>
2022-08-12 14:31:18 +0530

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyCalc;
+
+namespace HelloApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+        }
+    }
+}
